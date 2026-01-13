d77f632271f8d409dd7b7687c22a9bb6c34e0df2
Ho-jin98 <hohoj98@naver.com>
2026-01-13 12:25:06 +0900

    Calculrator step1 clear

---
+++ Calculrator/src/step2/App.java
@@ -0,0 +1,13 @@
+package step2;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Calculrator calculrator = new Calculrator();
+
+        int result = calculrator.calculrator(1,2);
+
+
+    }
+}
