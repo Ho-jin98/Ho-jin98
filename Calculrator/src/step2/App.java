9c2bbe067a6793968462ce9c225106f660ce9d89
Ho-jin98 <hohoj98@naver.com>
2026-01-15 12:53:39 +0900

    stpe2 Calculator 수정 반영

--- Calculrator/src/step2/App.java
+++ Calculrator/src/step2/App.java
@@ -1,40 +1,65 @@
 package step2;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+        Calculator calculator = new Calculator();
         Scanner sc = new Scanner(System.in);
         System.out.println("======" + "계산기를 실행합니다" + "======");
-        String str1 = " ";
-        while (str1.equals(" ")) {
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            int num1 = sc.nextInt();
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            int num2 = sc.nextInt();
-            if (num1 < 0 || num2 < 0) {
-                System.out.println("0 이상의 양의 정수를 입력해주세요.");
-                continue;
-            }
-            System.out.print("사칙 연산 기호를 입력하세요 (+,-,*,/): ");
-            char c = sc.next().charAt(0);
-            // TODO : Calculator 계산하는 기능 호출
+        String input1 = " ";
+        while (input1.equals(" ")) {
+            try {
+                System.out.print("첫 번째 숫자를 입력하세요: ");
+                int num1 = Integer.parseInt(sc.next());
+                System.out.print("두 번째 숫자를 입력하세요: ");
+                int num2 = Integer.parseInt(sc.next());
+                if (num1 < 0 || num2 < 0) {
+                    System.out.println("두 숫자 모두 0 이상의 양의 정수를 입력해주세요.");
+                    continue;
+                }
+                    System.out.print("사칙연산 기호(+, -, *, /)를 입력해주세요: ");
+                    char c = sc.next().charAt(0);
+                    if (c != '+' && c != '-' && c != '*' && c != '/') {
+                        System.out.println("사칙연산 기호(+, -, *, /)중 하나를 입력하세요.");
+                    continue;
+                    }
+                    //TODO : default가 클래스 안으로 들어가서 if문 활용
+                    //TODO : calculator 기능 호출
+                    int result = calculator.calculate(num1, num2, c);
+                    //TODO : 반환 값을 받아주는 상자를 만들어 줘야함, -> int result
+                    System.out.println("계산 결과: " + result);
+                    calculator.setResults(result);
+                    //TODO : 저장하는 기능을 호출하기
+                    //TODO : Calculator 클래스에 void로 출력문이 있음.
+                    System.out.print("첫 번째 결과 값을 삭제하겠습니까? (yes/no): ");
+                    String input3 = sc.next();
+                    if (input3.equalsIgnoreCase("yes")) {
+                        calculator.removeResults();
+                    } else if (input3.equalsIgnoreCase("no")) {
+                        System.out.println("현재 저장되어 있는 결과 값: " + calculator.getResults());
+                        //TODO : .getResults() > 게터를 사용하여 저장되어 있는 값들을 보여줌
+                    } else {
+                        System.out.println("yes와 no중에 입력해주세요");
+                        continue;
+                    }
 
-
-
-
-
-            System.out.print("계산을 계속 하겠습니까? (exit 입력시 종료): ");
-            String str2 = sc.next();
-            if (str2.equals("exit")) {
+            }catch(Exception e){
+                    System.out.println("숫자만 입력하세요");
+                    continue;
+                }
+            System.out.println("계산을 계속 하겠습니까?");
+            System.out.print("exit 입력시 종료, 계속 하려면 아무거나 입력: ");
+            String input2 = sc.next();
+            if (input2.equalsIgnoreCase("exit")) {
                 System.out.println("계산기 프로그램을 종료합니다");
-                break;}
+                break;
+            }
+    //TODO :  equalsIgnoreCase >> 대,소문자가 섞여 있어도 상관없이 처리해줌
+    //TODO :  .toLowerCase(); >> 들어오는 문자 모두 소문자로 바꿔줌
+    //TODO :  .toUpperCase(); >> 들어오는 문자 모두 대문자로 바꿔줌
+
         } sc.close();
-
-
-
-
 
     }
 }
--- Calculrator/src/step2/Calculator.java
+++ Calculrator/src/step2/Calculator.java
@@ -1,44 +1,77 @@
 package step2;
 
+import java.util.ArrayList;
+
 public class Calculator {
+    // 속성
+    private ArrayList<Integer> results;
+//TODO : ArrayList<Integer>를 results 변수 안에 넣어주는 것,
+//TODO : ArrayList<Integer>은 컬렉션에서 제공하는 클래스중 하나이다.
 
-    int num1;
-    int num2;
-    char c;
-    int total = (num1 + num2);
-
-
-    Calculator(int num1, int num2, char c) {
-        this.num1 = num1;
-        this.num2 = num2;
-        this.c = c;
-        this.total = (num1 + num2);
+    // 생성자
+    public Calculator() {
+        this.results = new ArrayList<>();
     }
 
-    public int calculator(int num1, int num2) {
-        total = (num1 + num2);
+    // 기능
+    public int calculate(int num1, int num2, char c) {
+        int result = 0;
         switch (c) {
             case '+':
-                System.out.println("덧셈 결과: " + (num1 + num2));
+                result = num1 + num2;
                 break;
             case '-':
-                System.out.println("뺄셈 결과: " + (num1 - num2));
+                result = num1 - num2;
                 break;
             case '*':
-                System.out.println("곱셈 결과: " + (num1 * num2));
+                result = num1 * num2;
                 break;
             case '/':
                 try {
-                    System.out.println("나눗셈 결과: " + (num1 / num2));
+                    result = num1 / num2;
                     break;
                 } catch (ArithmeticException e) {
                     System.out.println("0으로 나눌 수 없습니다");
                 }
+                break;
+
             default:
-                System.out.println("사칙연산 기호(+, -, *, /)중 하나를 입력해주세요.");
+                System.out.print("사칙연산 기호(+, -, *, /)를 입력해주세요: ");
         }
-        return total;
+        return result;
+    }
+
+    // TODO : 저장하는 기능 (세터 - 변경사항이 생김)
+    public void setResults(int result) {
+        this.results.add(result);
+        // TODO : results.add(result); >> this키워드를 사용하지 않았을 때 에러 발생
+        // TODO : IndexOutOfBoundsException >> 인덱스의 범위를 벗어난 에러
+        // TODO : Index 3 out of bounds for length 1
+        System.out.println("계산 결과 값 저장" + results);
+    }
+    //TODO : 조회는 저장된 값을 출력하면 됨.
+
+    // TODO : 삭제하는 기능
+    public void removeResults() {
+        this.results.remove(0);
+        System.out.println("첫 번째 결과 값이 삭제되었습니다");
+        System.out.println("현재 남아 있는 값: " + results);
+    }
+    //TODO : 가장 먼저 저장된 데이터 삭제 >> index 0번째
+    //TODO : this.results.remove(인덱스 ?번째로 입력)
+
+    //TODO : 게터 - 외부에서 속성을 참조할 수 있게 만들어 주는 기능
+    //TODO : 게터를 통해서 사용자가 저장된 값을 삭제하지 않았을 때 남아있는 값을 보여주기 위해
+    //TODO : 게터를 사용하여 속성을 참조해서 보여줌
+    public ArrayList<Integer> getResults() {
+        return results;
+    }
+    public static int parseInt(String input1, int num1){
+        try{
+            return Integer.parseInt(input1);
+        } catch (NumberFormatException e) {
+            return num1;
+        }
     }
 
 }
-
