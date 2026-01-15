ffdde6fbdd0ba524b3dcb877b8defa656c42336e
Ho-jin98 <hohoj98@naver.com>
2026-01-15 13:06:10 +0900

    stpe1 Calculator

--- Calculrator/src/step1/App.java
+++ Calculrator/src/step1/App.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+
         //TODO : 정수형 int를 0으로 나누게 되면 런타임 에러가 발생함,
         //TODO : 하지만 실수형 double은 0으로 나누어도 에러가 발생하지 않고, infinity 또는 NAM을 반환함
         //TODO : 또는 Try-catch 예외처리 블록 활용
@@ -47,6 +48,7 @@ public class App {
                     System.out.println("사칙연산 기호(+, -, *, /)중 하나를 입력해주세요.");
   //TODO : continue를 사용하여 사용자가 사칙연산 기호를 잘못 입력한 경우 "첫 번째 숫자를 입력하세요"로 돌아감
             }
+
             System.out.print("계산을 계속 하겠습니까? (exit 입력시 종료): ");
             String str2 = sc.next();
             if (str2.equals("exit")) {
